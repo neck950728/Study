35aa7b53c1080be69281b37893a8f1300c75300d
neck950728 <p_gon_ha_da@naver.com>
2024-01-04 11:32:29 +0900

    React - Router

--- study_react/src/index.js
+++ study_react/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './example/Styled-Components';
 import reportWebVitals from './reportWebVitals';
+
+import App from './example/Router';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
+    {/* <HashRouter><App /></HashRouter> */}
   </React.StrictMode>
 );
 
