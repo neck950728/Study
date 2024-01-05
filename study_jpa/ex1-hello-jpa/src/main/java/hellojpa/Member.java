1a817453c159868c80bec11b2d070235fe63ae9b
neck950728 <p_gon_ha_da@naver.com>
2024-01-05 22:08:12 +0900

    comment : JPA - 기본(Example 1) 파트 ≫ 주석 수정

--- study_jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ study_jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,13 +6,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity // JPA의 관리 대상이 되기 위한 애너테이션(final, enum, interface, inner 클래스에는 사용할 수 없다.)
-// @Table(name = "USER") // 클래스명과 테이블명이 다를 경우, @Table 애너테이션을 통해 직접 지정해줄 수 있다.
 public class Member {
     @Id // JPA에게 내 Primary Key가 무엇인지 알려주기 위한 애너테이션
     private Long id;
 
-    // @Column(name = "username") // 마찬가지로 필드명과 컬럼명이 다른 경우, 지정 방법
-    // @Column(unique = true, length = 10) // 제약 조건, 크기 등 추가적인 정보를 DDL 자동 생성 기능에 전달하기 위한 속성들(더 다양한 속성은 chapter2 참고)
     private String name;
 
     // DB에 저장할 필드에 final 키워드를 사용하면 안 된다.
