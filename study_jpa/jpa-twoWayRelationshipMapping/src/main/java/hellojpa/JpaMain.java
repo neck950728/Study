f8667176d93e38d361f1d174c60fdd97dd6b9953
neck950728 <p_gon_ha_da@naver.com>
2024-01-11 11:35:43 +0900

    JPA - 양방향 연관 관계

---
+++ study_jpa/jpa-twoWayRelationshipMapping/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,46 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            // member.setTeam(team);
+            member.changeTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+            for(Member m : members){
+                System.out.println(findTeam.getName() + " : " + m.getUsername());
+            }
+
+            tx.commit();
+        }catch(Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
