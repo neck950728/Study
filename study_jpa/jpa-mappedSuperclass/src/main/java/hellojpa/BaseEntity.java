c9c6379fd104f4ac7f2acf9256415f9ceaaf6854
neck950728 <p_gon_ha_da@naver.com>
2024-02-08 11:55:36 +0900

    JPA - Mapped Superclass

---
+++ study_jpa/jpa-mappedSuperclass/src/main/java/hellojpa/BaseEntity.java
@@ -0,0 +1,52 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/*
+    MappedSuperclass는 상속 관계 매핑 같은 것이 아니라,
+    단순히 자식 클래스에 공통으로 사용되는 매핑 정보를 '제공'해주는 역할만 한다.
+    ※Entity가 아니므로 테이블이 생성되지 않고, 당연히 조회 및 검색 또한 불가능하다.
+*/
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "INSERT_MEMBER")
+    private String createdBy;
+    private LocalDateTime createdDate;
+    @Column(name = "UPDATE_MEMBER")
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
