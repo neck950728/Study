5366d79fd7b5fff3957ff91d10367f3a4f67e63d
neck950728 <p_gon_ha_da@naver.com>
2024-06-17 15:26:09 +0900

    comment : JPA - 다대일(단방향) 연관 관계 매핑 파트 ≫ 주석 수정

--- study_jpa/jpa-oneWayRelationshipMapping/src/main/java/hellojpa/Member.java
+++ study_jpa/jpa-oneWayRelationshipMapping/src/main/java/hellojpa/Member.java
@@ -17,12 +17,8 @@ public class Member {
     */
 
     @ManyToOne // 다대일(N:1) 관계 - Many : Member, One : Team
-    /*
-        외래키로 사용할 컬럼을 지정
-        쉽게 말해, "내 컬럼 중 'TEAM_ID'라는 컬럼을 사용하여 Team 테이블의 PK와 JOIN하겠다."라는 의미
-    */
-    @JoinColumn(name = "TEAM_ID")
-    private Team team; // 객체 지향적 모델링 : 이전 위 방식과는 다르게 참조하는 방식임(외래키를 직접 다루지 않음)
+    @JoinColumn(name = "TEAM_ID") // 외래키와 필드를 매핑(+연관 관계의 주인이 됨 : 외래키를 관리(쓰기 작업 가능)함)
+    private Team team; // 객체 지향적 모델링 : 이전 위(15 ~ 16 Line) 방식과는 다르게 참조하는 방식임(외래키를 직접 다루지 않음)
 
     public Long getId() {
         return id;
