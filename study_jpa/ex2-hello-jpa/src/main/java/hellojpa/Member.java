6c3caeea21cda0a71ee7f1ec0b6fb736eda5fac8
Minjin Kim <151998896+neck950728@users.noreply.github.com>
2024-07-05 09:41:26 +0900

    comment : JPA - 기본(Example 2) 파트 ≫ 주석 수정

--- study_jpa/ex2-hello-jpa/src/main/java/hellojpa/Member.java
+++ study_jpa/ex2-hello-jpa/src/main/java/hellojpa/Member.java
@@ -31,7 +31,7 @@ public class Member {
     /*
         @Column(nullable = false, length = 10, ...) : name 외 속성
         DDL 자동 생성 기능 사용 시 제약 조건, 크기 등의 추가적인 정보를 전달
-        ※https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21693 : 6분 ~ 10분 32초 참고
+        ※https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21693 : 6분 ~ 10분 31초 참고
     */
     private String username;
 
