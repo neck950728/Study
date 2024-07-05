82e4aacb5d1027e122b8daf29571d004300e732f
Minjin Kim <151998896+neck950728@users.noreply.github.com>
2024-07-05 09:54:04 +0900

    comment : JPA - 다대일(양방향) 연관 관계 매핑 파트 ≫ 주석 수정

--- study_jpa/jpa-manyToOne(twoWay)-relationshipMapping/src/main/java/hellojpa/Member.java
+++ study_jpa/jpa-manyToOne(twoWay)-relationshipMapping/src/main/java/hellojpa/Member.java
@@ -43,7 +43,7 @@ public class Member {
 
     /*
         - 연관 관계 편의 메서드 -
-        ㆍ왜 굳이 양쪽 모두에 값을 세팅해야 하는 걸까? : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21698 4분 20초 ~ 10분 59초 참고
+        ㆍ왜 굳이 양쪽 모두에 값을 세팅해야 하는 걸까? : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21698 4분 20초 ~ 10분 58초 참고
         ㆍ연관 관계 편의 메서드는 어디에 정의해야 할까? : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21698 15분 21초 ~ 17분 57초 참고
     */
     public void changeTeam(Team team){
