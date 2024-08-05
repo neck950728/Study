9fe0c1fdc06377ae3b7da31d643c2c5d14cf2dab
Minjin Kim <151998896+neck950728@users.noreply.github.com>
2024-08-06 00:02:22 +0900

    comment : JPA - 상속 관계 매핑 파트 ≫ 주석 수정

--- study_jpa/jpa-inheritanceMapping/src/main/java/hellojpa/Item.java
+++ study_jpa/jpa-inheritanceMapping/src/main/java/hellojpa/Item.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 /*
     - 상속 관계 매핑 -
-    ㆍ전략 종류 : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21705 : 2분 50초 ~ 5분 16초 참고
+    ㆍ전략 종류 : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21705 2분 50초 ~ 5분 16초 참고
     ㆍ전략별 장단점 : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21705 30분 46초 ~ 37분 8초 참고
     ㆍ언제, 어떤 전략을 사용하는 것이 효율적일까? : https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21705 38분 17초 ~ 39분 20초 참고
 */
