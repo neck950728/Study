2bbff7e8319f3eb30f87d724100314a4784f99fd
neck950728 <p_gon_ha_da@naver.com>
2024-10-07 05:30:33 +0900

    JPA - 값 타입(임베디드 타입)

---
+++ study_jpa/jpa-embeddedType/src/main/java/hellojpa/Member.java
@@ -0,0 +1,81 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    /*
+        private LocalDateTime startDate;
+        private LocalDateTime endDate;
+    */
+    @Embedded
+    private Period workPeriod;
+
+    /*
+        private String city;
+        private String street;
+        private String zipcode;
+    */
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({ // https://www.inflearn.com/course/lecture?courseSlug=ORM-JPA-Basic&unitId=21713 : 16분 1초 ~ 16분 48초 참고
+            @AttributeOverride(name = "city",
+                    column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+                    column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                    column = @Column(name = "WORK_ZIPCODE")),
+    })
+    private Address workAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(Address workAddress) {
+        this.workAddress = workAddress;
+    }
+}
